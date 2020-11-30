820f95d2a1adcd1e0f9d290848a52bf936e92a06
Wilinton Vidal <wjaniervalvarez@misena.edu.co>
2020-11-29 21:41:10 -0500

    Cambiando letreros a loremIpsum

--- src/routes/quienessomos.svelte
+++ src/routes/quienessomos.svelte
@@ -11,12 +11,10 @@
     <h2 class="text-center">Quienes somos</h2>
     <div class="row">
         <div class="col-md-6">
-            <p>Ser una de las mejores firmas de abogados a nivel nacional con proyección internacional, es el resultado de la disciplina y pasión con la que hemos asumido más de quince años de gestión empresarial y trabajo en el derecho.
-                Bajo la dirección de un exitoso líder, como lo es el Doctor Abelardo De La Espriella y la guía de los Doctores Daniel Peñarredonda, Karen Juris, Carlos Sánchez, Wilson Caballero, Diana Bravo y Javier de la Hoz, socios de la firma, hemos logrado el posicionamiento de un completo y competente portafolio de servicios en asesoría, consultoría y representación legal, un equipo interdisciplinario altamente calificado, los éxitos jurídicos de mayor impacto para la opinión pública y ser los grandes protagonistas en el ejercicio del derecho en Colombia.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div class="col-md-6">
-            <p>Nuestro prestigio es consecuencia de las ventajas competitivas que nos permiten sobresalir, entre las que cabe resaltar, la calificada experiencia de sus directivos, la orientación al éxito en cada caso, la certificación de calidad Bureau Veritas iso 9001, el compromiso ético y deontológico, la asesoría multidisciplinaria y la red nacional e internacional de servicios.
-                Para responder a sus requerimientos contamos con centros de operación en Barranquilla, Bogotá, Medellín y Miami, e igualmente con alianzas estratégicas a lo largo del territorio nacional y en el exterior, lo que nos hace líderes en nuestras áreas de práctica.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
     </div>
     <div class="row">
@@ -25,14 +23,13 @@
         </div>
         <div class="col-md-6 align-middle">
             <h3>Misión</h3>
-            <p>DE LA ESPRIELLA Lawyers Enterprise®, Consultorías y Servicios Legales Especializados es una firma de abogados que con un enfoque multidisciplinario asesora, representa y brinda soluciones jurídicas integrales y de valor a personas naturales, así como entidades públicas y privadas, en un marco de políticas de excelencia, calidad y responsabilidad.
-            </p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-6">
             <h3>Visión</h3>
-            <p>En el 2022, DE LA ESPRIELLA Lawyers Enterprise®, Consultorías y Servicios Legales Especializados, liderará la prestación de servicios jurídicos en Colombia y se posicionará a través de alianzas estratégicas como una de las mejores y más eficientes firmas de abogados en el mercado internacional.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div class="col-md-6">
             <img src="Justicia.jpg" alt="">
