3f60efe0227ce643c59db0aa93b150c5d87aae5c
Wilinton Vidal <wjaniervalvarez@misena.edu.co>
2020-12-14 11:26:37 -0500

    Quitando lema

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -192,10 +192,10 @@
 			<br>
 			<h3>En todas las areas del derecho</h3>
 			<br>
-			<h5>Ustedes son la causa más importante,</h5>
+			<!-- <h5>Ustedes son la causa más importante,</h5>
 			<h5>la batalla que siempre estamos dispuestos a dar</h5>
 			<br>
-			<p>Nuestra filosofía está orientada a la prestación de servicios de asesoría, consultoría preventiva y resolución de problemas. Para eso trabajamos con rigurosidad y disciplina en las siguientes áreas del derecho:</p>
+			<p>Nuestra filosofía está orientada a la prestación de servicios de asesoría, consultoría preventiva y resolución de problemas. Para eso trabajamos con rigurosidad y disciplina en las siguientes áreas del derecho:</p> -->
 		</div>
 		<div class="col-md-7">
 			<div class="Posts row">
